0dd5844f3a1167913b0fda512e5d9983c1603e24
Rohit Khamkar <rohit.khamkar@rackspace.com>
2015-10-20 23:32:14 -0700

    Added copyright information to source files

--- RAX/Cloud/src/com/rax/deployment/UpdateTopologyFormHandler.java
+++ RAX/Cloud/src/com/rax/deployment/UpdateTopologyFormHandler.java
@@ -1,3 +1,30 @@
+/**
+ 	#################### COPYRIGHT #####################################
+    Copyright 2001, 2015, Rackspace Inc and/or its affiliates. All rights 
+    reserved.
+  	UNIX is a registered trademark of The Open Group.
+  	This software and related documentation are provided under a license
+  	agreement containing restrictions on use and disclosure and are
+  	protected by intellectual property laws. Except as expressly permitted
+  	in your license agreement or allowed by law, you may not use, copy,
+  	reproduce, translate, broadcast, modify, license, transmit, distribute,
+  	exhibit, perform, publish, or display any part, in any form, or by any
+  	means. Reverse engineering, disassembly, or decompilation of this
+  	software, unless required by law for interoperability, is prohibited.
+  	The information contained herein is subject to change without notice
+  	and is not warranted to be error-free. If you find any errors, please
+  	report them to us in writing.
+
+  	This software or hardware and documentation may provide access to or
+  	information on content, products, and services from third parties.
+  	Rackspace Inc, and its affiliates are not responsible for and
+  	expressly disclaim all warranties of any kind with respect to
+  	third-party content, products, and services. Rackspace Inc. and
+  	its affiliates will not be responsible for any loss, costs, or damages
+  	incurred due to your access to or use of third-party content, products,
+  	or services.
+ */
+
 package com.rax.deployment;
 
 import java.io.IOException;
